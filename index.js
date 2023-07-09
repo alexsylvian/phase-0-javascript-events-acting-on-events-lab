b97008a867025f2b10106b47d6c1f77ac02f32c8
alexsylvian <lazeratus1@gmail.com>
2023-07-09 15:05:44 -0400

    move dodger - PRACTICE!

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`
+    }
+}
+
+/*function moveDodgerleft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    const containerWidth = 300; // Set the maximum left position of the container
+  
+    if (left < containerWidth) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }*/
